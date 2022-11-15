db37b506ae370a3988898853a0fbb289489032e2
rud4038 <mm453@naver.com>
2022-11-15 12:21:53 +0900

    챕터1 출력

---
+++ src/main/java/j01_출력/SystemOut.java
@@ -0,0 +1,14 @@
+package j01_출력;
+
+public class SystemOut {
+    public static void main(String[] args) {
+        System.out.print("이름: ");
+        System.out.println("신경수");
+        System.out.print("주소: ");
+        System.out.println("부산 동래구 사직동");
+        System.out.print("연락처: ");
+        System.out.println("010-9941-4038");
+        System.out.print("성별: ");
+        System.out.println("남성");
+    }
+}
---
+++ src/main/java/j01_출력/UserInfo.java
@@ -0,0 +1,15 @@
+package j01_출력;
+
+public class UserInfo {
+    public static void main(String[] args) {
+        String name = "신경수";
+        String address = "부산 사하구 장림동";
+        String phone = "010-9941-4038";
+        int age = 25;
+
+
+        System.out.println(name + "의 주소는 " + address + "입니다.");
+        System.out.println(name + "의 연락처는 " + phone + "입니다.");
+        System.out.println(name + "의 나이는 " + age + "살입니다.");
+    }
+}
