8ddeb713b692d0899dae59b65667bc0b422f7a0f
rud4038 <mm453@naver.com>
2022-12-05 12:54:11 +0900

    챕터20 함수형인터페이스

--- src/main/java/j20_함수형인터페이스/Main3.java
+++ src/main/java/j20_함수형인터페이스/Main3.java
@@ -1,7 +1,13 @@
 package j20_함수형인터페이스;
 
+
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 import java.util.function.Consumer;
 
 public class Main3 {
@@ -29,6 +35,31 @@ public class Main3 {
 		
 		forEachTest(n -> {System.out.println(n);}, names);
 		
+		Map<String, String> subject = new HashMap<>();
+		
+		subject.put("java", "자바");
+		subject.put("python", "파이썬");
+		subject.put("javascript", "자바스크립트");
+		subject.put("C", "씨언어");
+		subject.put("Nods.js", "노드제이에스");
+		
+		Set<String> keySet = subject.keySet();
+		keySet.forEach(key -> {
+			
+			System.out.println(subject.get(key));
+		});
+		System.out.println();
+		
+		Set<Entry<String, String>> entrySet = subject.entrySet();
+		
+		entrySet.forEach(entry -> {
+			System.out.println(entry.getKey() + " : " + entry.getValue());
+		});
+		System.out.println();
+		
+		subject.forEach((key, value) -> {
+			System.out.println(key + " : " + value);
+		});
 		
 	}
 
